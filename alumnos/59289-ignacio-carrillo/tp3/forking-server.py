ca559fc1125c459a04dcc3b49fc9ad19a5295df5
Gustavo Ignacio Carrillo <carrillo.ignaciog@gmail.com>
2020-09-27 20:52:15 -0300

    fork, filter

--- alumnos/59289-ignacio-carrillo/tp3/forking-server.py
+++ alumnos/59289-ignacio-carrillo/tp3/forking-server.py
@@ -1,6 +1,8 @@
 import socketserver
 import os
 from parser import parser
+import math
+from filter import *
 
 class Handler(socketserver.BaseRequestHandler):
 
@@ -10,28 +12,65 @@ class Handler(socketserver.BaseRequestHandler):
             path=os.getcwd()
             if required =="/":
                 path=f"{path}/index.html"
-            elif required=="/dog.ppm":
-                path=f"{path}/selector.html"
             else:
                 path=f"{path}{required}"
 
-            path,header=self.generateHeader(path)
-
-            try:
+            if  "dog.ppm?" in path:
+                filter, int = self.queryAnalizer(path)
+                path=f"{os.getcwd()}/dog.ppm"
                 fdr=os.open(path,os.O_RDONLY)
-                first_it=True
-                while True:
-                    body=os.read(fdr,arguments.size)
-                    to_send=body
-                    if first_it:
-                        to_send = header + body
-                        first_it=False
-                    self.request.sendall(to_send)
-                    if (len(body)!=arguments.size):
-                        os.close(fdr)
-                        break
-            except IOError:
-                print("Error al abrir el archivo")
+                search=os.read(fdr,1024)
+                offset=self.offsetAnalyzer(search)
+                header_ppm=search[:offset]
+                # threads_quantity=math.ceil(((os.stat(path).st_size-offset)-offset)/arguments.size)
+                # #os.lseek(fdr,offset+indiceHILO*argumento.size,0)
+
+            path,header= self.generateHeader(path)
+            self.writeData(path,header)
+
+    def offsetAnalyzer(self, block):
+        lines = block.splitlines()
+        offset = 0
+
+        for line in lines:
+            if line == b'P6':
+                offset += len(line) + 1
+            elif line[0] == ord('#'):
+                offset += len(line) + 1
+            elif len(line.split()) == 2:
+                offset += len(line) + 1
+            else:
+                offset += len(line) + 1
+                break
+
+        return offset
+
+    def writeData(self,path, header):
+        try:
+            fdr = os.open(path, os.O_RDONLY)
+            first_it = True
+            while True:
+                body = os.read(fdr, arguments.size)
+                to_send = body
+                if first_it:
+                    to_send = header + body
+                    first_it = False
+                self.request.sendall(to_send)
+                if (len(body) != arguments.size):
+                    os.close(fdr)
+                    break
+
+        except IOError:
+            print("Error al abrir el archivo")
+
+    def queryAnalizer(self,path):
+        val_dict={}
+        querys=(path.split("?")[1]).split("&")
+        values=[value.split("=") for value in querys]
+        for i in range(len(values)):
+            val_dict[values[i][0]]=values[i][1]
+
+        return val_dict["filter"],int(val_dict["Intensity"])
 
     def generateHeader(self,path):
         mime={"jpg":"image/jpeg","pdf":"application/pdf","html":"text/html","ppm": "image/x-portable-pixmap"}
@@ -39,7 +78,7 @@ class Handler(socketserver.BaseRequestHandler):
             fline="HTTP/1.1 200 OK"
         else:
             path=f"{os.getcwd()}/error.html"
-            fline ="HTTP/1.1 400 File not found"
+            fline ="HTTP/1.1 404 Not Found"
         extension=path.split(".")[1]
         size=os.stat(path).st_size
         header="{}\r\nContent-type: {}\r\nContent-lenght: {}\r\n\r\n".format(fline,mime[extension],size)
@@ -56,9 +95,11 @@ class Handler(socketserver.BaseRequestHandler):
 if __name__ == '__main__':
     os.system("reset")
     arguments=parser()
+    if arguments.size % 3 != 0:
+        arguments.size += (3-(arguments.size % 3))
     print("Iniciando server...")
     socketserver.ForkingTCPServer.allow_reuse_address = True
-    HOST, PORT = 'localhost', 5000
+    HOST, PORT = 'localhost', arguments.port
     with socketserver.ForkingTCPServer((HOST, PORT), Handler) as server:
         server.serve_forever()
 
